4dcee2af47b1db39eaff3e6e42974b2b89318c49
Mario Bustamante <bustamantemario67@gmail.com>
2025-08-25 12:11:44 -0500

    FRACTIONAL TULUM V0.1.5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,8 @@
-"use client";
-
 import type { Metadata } from "next";
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { useTheme } from "next-themes";
-import { useEffect } from "react";
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,12 +20,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { theme, resolvedTheme } = useTheme();
-
-  useEffect(() => {
-    console.log("Current theme:", theme);
-    console.log("Resolved theme:", resolvedTheme);
-  }, [theme, resolvedTheme]);
 
   return (
     <html lang="es" suppressHydrationWarning>
